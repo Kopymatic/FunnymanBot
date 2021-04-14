a65b9aa30551e2cfd429069c85da651bc93489c0
Kopymatic <creepcipher@gmail.com>
2021-04-14 17:06:21 -0600

    Reference utility for common values

---
+++ src/main/kotlin/kotBot/utils/Reference.kt
@@ -0,0 +1,9 @@
+package kotBot.utils
+
+import java.awt.Color
+
+data class Reference (
+    val botName: String = "KotBot",
+    val version: String = "Beta 1",
+    val defaultColor: Color = Color(255, 111, 255)
+)
