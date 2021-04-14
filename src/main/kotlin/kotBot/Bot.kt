34131a4854295773d8ded52243988506dfd2d182
Kopymatic <creepcipher@gmail.com>
2021-04-13 23:38:47 -0600

    Bot.kt - numerous changes
    Make the bot get the token from a data class called Config
    
    make a new KopyDB utility thing
    
    add new LoveCommands

--- src/main/kotlin/kotBot/Bot.kt
+++ src/main/kotlin/kotBot/Bot.kt
@@ -3,19 +3,24 @@ package kotBot
 import com.jagrosh.jdautilities.command.CommandClient
 import com.jagrosh.jdautilities.command.CommandClientBuilder
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter
+import kotBot.commands.loveCommands.LoveCommands
+import kotBot.utils.Config
+import kotBot.utils.KopyDB
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.utils.cache.CacheFlag
 
-fun main(args: Array<String>) {
-    Bot().createBot(args[0])
+fun main() {
+    Bot().createBot(Config().token)
+    //TODO make this load from a JSON config file
 }
 
 class Bot() {
     lateinit var jda: JDA
     lateinit var commandClient: CommandClient
+    val kdb = KopyDB()
     val waiter = EventWaiter()
 
     fun createBot(token: String) {
@@ -30,7 +35,11 @@ class Bot() {
             .setEmojis("\u2705", "\uD83D\uDE2E", "\uD83D\uDE26")
             .setPrefix("kk")
             .setAlternativePrefix("k!")
-            .addCommands()
+            .addCommands(
+                LoveCommands.Hug(),
+                LoveCommands.Kiss(),
+                LoveCommands.Cuddle()
+            )
             .build()
 
         jda.addEventListener(commandClient)
