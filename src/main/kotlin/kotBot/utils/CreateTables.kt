e04d07726bc4c09148223f4e27d8d1e73682cdfb
Kopymatic <creepcipher@gmail.com>
2021-04-15 00:00:27 -0600

    Add new NoContext table

--- src/main/kotlin/kotBot/utils/CreateTables.kt
+++ src/main/kotlin/kotBot/utils/CreateTables.kt
@@ -14,7 +14,31 @@ fun main() {
         stmt.executeUpdate(sql)
         stmt.close()
         c.close()
+        println("Success!")
     } catch ( e: Exception) {
-        println("Already Exists!")
+        println(e.message)
+    }
+
+    try {
+        val c = DriverManager.getConnection(Config().url, Config().userName, Config().password)
+        val stmt = c.createStatement()
+        val sql =
+            """
+                CREATE TABLE NoContext(
+                    id SERIAL PRIMARY KEY,
+                    guildID TEXT NOT NULL,
+                    ImageLink TEXT NOT NULL,
+                    LinkTag TEXT,
+                    TextTag TEXT,
+                    ImporterID TEXT NOT NULL,
+                    ImportMessageID TEXT NOT NULL
+                );
+            """.trimIndent()
+        stmt.executeUpdate(sql)
+        stmt.close()
+        c.close()
+        println("Success!")
+    } catch ( e: Exception) {
+        println(e.message)
     }
 }
