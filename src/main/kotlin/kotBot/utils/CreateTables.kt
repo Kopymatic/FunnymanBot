f0e6802f40c3db07248ac1ca97caed1af3cbcb06
Kopymatic <creepcipher@gmail.com>
2021-04-18 03:49:20 -0600

    Table setup for nc, ppl, meme, pet

--- src/main/kotlin/kotBot/utils/CreateTables.kt
+++ src/main/kotlin/kotBot/utils/CreateTables.kt
@@ -3,27 +3,19 @@ package kotBot.utils
 import java.sql.DriverManager
 
 fun main() {
-    try {
-        val c = DriverManager.getConnection(Config().url, Config().userName, Config().password)
-        val stmt = c.createStatement()
-        val sql = "CREATE TABLE LoveCommands " +
-             "(SenderID           TEXT  NOT NULL," +
-             " ReceiverID         TEXT  NOT NULL, " +
-             " ActionIdentifier   TEXT  NOT NULL, " +
-             " TimesPerformed     INT   NOT NULL)"
-        stmt.executeUpdate(sql)
-        stmt.close()
-        c.close()
-        println("Success!")
-    } catch ( e: Exception) {
-        println(e.message)
-    }
+    create( //Create LoveCommands
+        """
+            CREATE TABLE LoveCommands(
+            SenderID TEXT NOT NULL,
+            ReceiverID TEXT NOT NULL,
+            ActionIdentifier TEXT NOT NULL,
+            TimesPerformed INT NOT NULL
+            );
+        """.trimIndent()
+    )
 
-    try {
-        val c = DriverManager.getConnection(Config().url, Config().userName, Config().password)
-        val stmt = c.createStatement()
-        val sql =
-            """
+    create( //Create NoContext
+        """
                 CREATE TABLE NoContext(
                     id SERIAL PRIMARY KEY,
                     guildID TEXT NOT NULL,
@@ -34,11 +26,60 @@ fun main() {
                     ImportMessageID TEXT NOT NULL
                 );
             """.trimIndent()
+    )
+
+    create( //Create People
+        """
+                CREATE TABLE People(
+                    id SERIAL PRIMARY KEY,
+                    guildID TEXT NOT NULL,
+                    ImageLink TEXT NOT NULL,
+                    LinkTag TEXT,
+                    TextTag TEXT,
+                    ImporterID TEXT NOT NULL,
+                    ImportMessageID TEXT NOT NULL
+                );
+            """.trimIndent()
+    )
+
+    create( //Create Pets
+        """
+                CREATE TABLE Pets(
+                    id SERIAL PRIMARY KEY,
+                    guildID TEXT NOT NULL,
+                    ImageLink TEXT NOT NULL,
+                    LinkTag TEXT,
+                    TextTag TEXT,
+                    ImporterID TEXT NOT NULL,
+                    ImportMessageID TEXT NOT NULL
+                );
+            """.trimIndent()
+    )
+
+    create( //Create Meme
+        """
+                CREATE TABLE Memes(
+                    id SERIAL PRIMARY KEY,
+                    guildID TEXT NOT NULL,
+                    ImageLink TEXT NOT NULL,
+                    LinkTag TEXT,
+                    TextTag TEXT,
+                    ImporterID TEXT NOT NULL,
+                    ImportMessageID TEXT NOT NULL
+                );
+            """.trimIndent()
+    )
+}
+
+fun create(sql: String) {
+    try {
+        val c = DriverManager.getConnection(Config().url, Config().userName, Config().password)
+        val stmt = c.createStatement()
         stmt.executeUpdate(sql)
         stmt.close()
         c.close()
         println("Success!")
-    } catch ( e: Exception) {
+    } catch (e: Exception) {
         println(e.message)
     }
 }
