c49be46ab0ef9ed0499da2286d4498daf42117bb
Kopymatic <creepcipher@gmail.com>
2021-04-18 03:48:57 -0600

    Rearranging in Bot.kt

--- src/main/kotlin/kotBot/Bot.kt
+++ src/main/kotlin/kotBot/Bot.kt
@@ -1,49 +1,48 @@
 package kotBot
 
-import com.jagrosh.jdautilities.command.CommandClient
 import com.jagrosh.jdautilities.command.CommandClientBuilder
-import com.jagrosh.jdautilities.commons.waiter.EventWaiter
-import kotBot.commands.`fun`.loveCommands.LoveCommand
+import kotBot.commands.`fun`.LoveCommand
+import kotBot.commands.`fun`.randomImageCommands.MemeCmd
 import kotBot.commands.`fun`.randomImageCommands.NoContextCmd
+import kotBot.commands.`fun`.randomImageCommands.PeopleCmd
+import kotBot.commands.`fun`.randomImageCommands.PetCmd
+import kotBot.commands.util.EmbedCmd
 import kotBot.utils.Config
-import kotBot.utils.KopyDB
-import net.dv8tion.jda.api.JDA
+import kotBot.utils.Reference
 import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.requests.GatewayIntent
 import net.dv8tion.jda.api.utils.cache.CacheFlag
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter as EventWaiter
 
 fun main() {
     Bot().createBot(Config().token)
     //TODO make this load from a JSON config file
 }
 
-class Bot() {
-    lateinit var jda: JDA
-    lateinit var commandClient: CommandClient
-    val kdb = KopyDB()
-    val waiter = EventWaiter()
-
+class Bot {
     fun createBot(token: String) {
-        jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS)
-            .disableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOTE)
-            .addEventListeners(waiter)
-            .build()
-
-        commandClient = CommandClientBuilder()
-            .setActivity(Activity.watching("A")) //This don't work
+        Reference.waiter = EventWaiter()
+        Reference.cmdClient = CommandClientBuilder()
+            .setActivity(Reference.status)
             .setOwnerId("326489320980611075")
             .setEmojis("\u2705", "\uD83D\uDE2E", "\uD83D\uDE26")
-            .setPrefix("kk")
-            .setAlternativePrefix("k!")
+            .setPrefix(Reference.mainPrefix)
+            .setAlternativePrefix(Reference.alternativePrefix)
             .addCommands(
+                NoContextCmd(),
+                PeopleCmd(),
+                PetCmd(),
+                MemeCmd(),
                 LoveCommand.Hug(),
                 LoveCommand.Kiss(),
                 LoveCommand.Cuddle(),
-                NoContextCmd()
+                EmbedCmd()
             )
             .build()
 
-        jda.addEventListener(commandClient)
+        Reference.jda = JDABuilder.createDefault(token, GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS)
+            .disableCache(CacheFlag.VOICE_STATE, CacheFlag.EMOTE)
+            .addEventListeners(Reference.waiter, Reference.cmdClient)
+            .build()
     }
 }
