c548a133083be538c6e03fd3325cd9e1d59f547f
Kopymatic <creepcipher@gmail.com>
2021-05-02 21:55:46 -0600

    add sex alarm

--- src/main/kotlin/kotBot/utils/EveryMessageManager.kt
+++ src/main/kotlin/kotBot/utils/EveryMessageManager.kt
@@ -11,9 +11,19 @@ class EveryMessageManager: ListenerAdapter() {
 
         if (dylanMode && event.message.referencedMessage != null) {
             val referencedMessage = event.message.referencedMessage!!
-            event.channel.sendMessage("**${event.member?.effectiveName}** replied to **${referencedMessage.member?.effectiveName}**\n" +
-                    "> *${referencedMessage.contentStripped.replace("\n", "\n> ").replace("@everyone", "everyone").replace("@here", "here")}*\n" + event.message.contentRaw
+            event.channel.sendMessage(
+                "**${event.member?.effectiveName}** replied to **${referencedMessage.member?.effectiveName}**\n" +
+                        "> *${
+                            referencedMessage.contentStripped.replace("\n", "\n> ").replace("@everyone", "everyone")
+                                .replace("@here", "here")
+                        }*\n" + event.message.contentRaw
             ).queue()
+        }
+
+        if (event.message.contentRaw.contains("sex")) {
+            val channel = event.jda.textChannels.find { channel -> channel.name == "sex-alarm" }
+            channel?.sendMessage("${event.member?.effectiveName} has sexed in <#${event.channel.id}>!!!! :flushed:")
+                ?.queue()
         }
     }
 }
