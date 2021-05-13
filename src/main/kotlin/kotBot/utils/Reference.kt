d6e46efc82c54de0254c2aef43e75d11a9b06e95
Kopymatic <creepcipher@gmail.com>
2021-05-12 19:53:46 -0600

    command to allow for remote changing of some variables, update version to 4.3

--- src/main/kotlin/kotBot/utils/Reference.kt
+++ src/main/kotlin/kotBot/utils/Reference.kt
@@ -2,25 +2,25 @@ package kotBot.utils
 
 import com.jagrosh.jdautilities.command.Command
 import com.jagrosh.jdautilities.command.CommandClient
+import com.jagrosh.jdautilities.command.CommandEvent
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter
-import jdk.jfr.Event
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.entities.Activity
 import java.awt.Color
-import java.time.format.DateTimeFormatterBuilder
-
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
 
 class Reference {
     companion object {
         const val experimental = true
-        const val botName = "KotBot"
-        const val version = "4.2"
+        const val botName = "Funnyman"
+        var version: String = "4.3"
         val token = if (!experimental) Config().mainToken else Config().devToken
         val status = Activity.watching("V$version ${if (experimental) "Experimental" else ""}")
         val ownerID = "326489320980611075"
         val dateFormatter: DateTimeFormatter =
             DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("L/d/yy h:mm a").toFormatter()
+        var doTyping: Boolean = true //This determines if the bot will send a typing status on every command
 
         //Categories!
         val convenienceCategory = Command.Category("Convenience")
@@ -29,16 +29,62 @@ class Reference {
         val quickStringCategory = Command.Category("QuickStringCommands")
         val categories = arrayOf(funCategory, convenienceCategory, utilityCategory, quickStringCategory)
 
-        val mainPrefix = if(!experimental) "pp" else "dd"
-        val alternativePrefix = if(!experimental) "p!" else "d!"
+        val mainPrefix = if (!experimental) "pp" else "dd"
+        val alternativePrefix = if (!experimental) "p!" else "d!"
         val prefixes = arrayOf(mainPrefix, alternativePrefix)
-        val defaultColor: Color = Color(255, 111, 255)
-        val rgb: Int = Color(255, 111, 255).rgb
+        var defaultColor: Color = Color(255, 111, 255)
+        var rgb: Int = defaultColor.rgb
 
         val kdb = KopyDB()
+        val connection = kdb.connection
         lateinit var jda: JDA
         val waiter: EventWaiter = EventWaiter()
         lateinit var cmdClient: CommandClient
-        val everyMessageManager = EveryMessageManager()
+        val everyMessageManager = EverythingListener()
     }
+
+    class ConfigCmd : KopyCommand() {
+        init {
+            name = "Config"
+            arguments = "no args for help"
+            help = "Change configurable variables"
+            guildOnly = false
+            hidden = true
+            ownerCommand = true
+            category = utilityCategory
+        }
+
+        override suspend fun onCommandRun(event: CommandEvent, guildSettings: GuildSettings) {
+            if (event.args.isBlank()) {
+                event.reply(
+                    """
+                    Current variables:
+                    ```kotlin
+                    doTyping: Boolean = $doTyping
+                    version: String = $version
+                    defaultColor: Color (rgb format) = ${defaultColor.red}, ${defaultColor.green}, ${defaultColor.blue}
+                    ```
+                    Change them with `${mainPrefix}${this.name} [variable] = [assignment]`
+                """.trimIndent()
+                )
+            } else {
+                val args = event.args.split("=")
+                if (args.size < 2) {
+                    event.reply("Not enough args - make sure to remember the equal sign"); return
+                }
+                when (args[0].trim()) {
+                    "doTyping" -> doTyping = args[1].toBoolean()
+                    "version" -> version = args[1]
+                    "defaultColor" -> {
+                        val rgb = args[1].split(",")
+                        defaultColor = Color(rgb[0].trim().toInt(), rgb[1].trim().toInt(), rgb[2].trim().toInt())
+                        Reference.rgb = defaultColor.rgb
+                    }
+                    else -> event.reply("Variable not found")
+                }
+                event.reactSuccess()
+            }
+        }
+    }
+
 }
