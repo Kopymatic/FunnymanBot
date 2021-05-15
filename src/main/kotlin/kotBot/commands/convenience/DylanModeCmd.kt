c45cb5ff03a6a4eaf44b0b4ddab0497b9bcbc8ba
Kopymatic <creepcipher@gmail.com>
2021-05-14 22:33:47 -0600

    Update dylanmode to use guildSettings instead of a global variable

--- src/main/kotlin/kotBot/commands/convenience/DylanModeCmd.kt
+++ src/main/kotlin/kotBot/commands/convenience/DylanModeCmd.kt
@@ -9,7 +9,6 @@ import net.dv8tion.jda.api.Permission
 class DylanModeCmd : KopyCommand() {
     init {
         name = "DylanMode"
-        aliases = arrayOf("")
         help = "Dylan mode makes all replies into quotes, this toggles it on and off"
         guildOnly = false
         userPermissions = arrayOf(
@@ -20,8 +19,9 @@ class DylanModeCmd : KopyCommand() {
     }
 
     override suspend fun onCommandRun(event: CommandEvent, guildSettings: GuildSettings) {
-        Reference.everyMessageManager.dylanMode = !Reference.everyMessageManager.dylanMode
-        if (Reference.everyMessageManager.dylanMode) {
+        guildSettings.dylanMode = !guildSettings.dylanMode
+        guildSettings.push()
+        if (guildSettings.dylanMode) {
             event.reply("Dylan mode active")
         } else {
             event.reply("Dylan mode inactive")
